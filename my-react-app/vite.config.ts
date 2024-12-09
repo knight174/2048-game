3757ae9985b6f11f12e1d8d18a2b0286fabd87e1
pinchao <cpc4253@163.com>
2024-12-09 13:36:32 +0800

    fix: 修改部署 base，支持 netlify

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2048-game/',
   plugins: [react()],
+  base: process.env.BASE_URL || '/',
 });
